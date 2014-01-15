f2c7592b22abb31b335db8404fa54ff0341cc1f7
Patrick Debois <Patrick.Debois@jedi.be>
2014-01-15 16:32:13 +0100

    first steps for buffer actions

---
+++ test/actions.js
@@ -0,0 +1,91 @@
+describe('terminal buffer actions', function () {
+
+  var a = TerminalParser.actions;
+  it('cursor_move', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 0, y: 0 };
+    var n = a.cursor_move(buffer, 0, 1);
+    expect(n.cursor.x).to.be(0);
+    expect(n.cursor.y).to.be(1);
+    done();
+
+  });
+
+  it('cursor_save', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 0, y: 4 };
+    var n = a.cursor_save(buffer);
+    expect(n.ocursor.x).to.be(0);
+    expect(n.ocursor.y).to.be(4);
+    done();
+
+  });
+
+  it('cursor_save with different ocursor', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 1, y: 3 };
+    buffer.ocursor = { x: 2, y: 4 };
+    var n = a.cursor_save(buffer);
+    expect(n.ocursor.x).to.be(1);
+    expect(n.ocursor.y).to.be(3);
+    done();
+
+  });
+
+  it('cursor_restore', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 1, y: 3 };
+    buffer.ocursor = { x: 2, y: 4 };
+    var n = a.cursor_restore(buffer);
+    expect(n.cursor.x).to.be(2);
+    expect(n.cursor.y).to.be(4);
+    done();
+
+  });
+
+  it('detects if at last cols', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 79, y: 2 };
+    buffer.size = { cols: 80 , rows: 3};
+    var n = a.isAtLastCol(buffer);
+    expect(n).to.be(true);
+    done();
+
+  });
+
+  it('detects if at last row', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 79, y: 2 };
+    buffer.size = { cols: 80 , rows: 3};
+    var n = a.isAtLastRow(buffer);
+    expect(n).to.be(true);
+    done();
+
+  });
+
+  it('inserts standard text', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 0, y: 0 };
+    buffer.size = { cols: 4 , rows: 2};
+    buffer.lines = [];
+    var n = a.insertText(buffer, 'a longer text i');
+    expect(n.lines.length).to.be(2);
+    expect(n.lines[0]).to.eql([ ' ' , 't', 'e', 'x']);
+    expect(n.lines[1]).to.eql([ 't' , ' ', 'i']);
+    expect(n.cursor.x).to.be(3);
+    expect(n.cursor.y).to.be(1);
+    done();
+
+  });
+
+  it('is Beyond Last Row', function(done) {
+    var buffer = {};
+    buffer.cursor = { x: 0, y: 0 };
+    buffer.size = { cols: 4 , rows: 2};
+    var n = a.isBeyondLastRow(buffer, 2);
+    expect(n).to.be(true);
+    done();
+
+  });
+
+});
--- test/buffer-stream.js
+++ test/buffer-stream.js
@@ -1,12 +1,13 @@
 describe('terminal buffer stream', function () {
 
-  it('can handle a string of plain characters', function(done) {
-    var stream = new TerminalParser.BufferStream();
-
     var text = 'beepboop';
     var esc = '\x1b';
     var csi = '\x1b[';
     var csiA = '\x1b[1A';
+
+  it.skip('can handle a string of plain characters', function(done) {
+    var stream = new TerminalParser.BufferStream();
+
 
     var count = 0;
     var t = [
@@ -15,14 +16,16 @@ describe('terminal buffer stream', function () {
     ];
 
     stream.on('data', function(buffer) {
-        expect(buffer).not.to.be(null);
+      count++;
+      if (count === t.length) {
+        expect(buffer).to.be(null);
         expect(buffer.cursor).not.to.be(null);
         done();
+       }
     });
 
     stream.write(t.join());
     stream.end();
   });
-
 
 });
