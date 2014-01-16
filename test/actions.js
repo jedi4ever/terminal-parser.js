6a55140d806f3e0ab154667dc6bec69558c79e0b
Patrick Debois <Patrick.Debois@jedi.be>
2014-01-16 18:55:28 +0100

    re-arrange opcodes to separate files

--- test/actions.js
+++ test/actions.js
@@ -1,31 +1,31 @@
 describe('terminal buffer actions', function () {
 
   var a = TerminalParser.actions;
-  it('cursor_move', function(done) {
+  it('moves the Cursor', function(done) {
     var buffer = {};
     buffer.cursor = { x: 0, y: 0 };
-    var n = a.cursor_move(buffer, 0, 1);
+    var n = a.moveCursor(buffer, 0, 1);
     expect(n.cursor.x).to.be(0);
     expect(n.cursor.y).to.be(1);
     done();
 
   });
 
-  it('cursor_save', function(done) {
+  it('saves the Cursor', function(done) {
     var buffer = {};
     buffer.cursor = { x: 0, y: 4 };
-    var n = a.cursor_save(buffer);
+    var n = a.saveCursor(buffer);
     expect(n.ocursor.x).to.be(0);
     expect(n.ocursor.y).to.be(4);
     done();
 
   });
 
-  it('cursor_save with different ocursor', function(done) {
+  it('saves the Cursor with different ocursor', function(done) {
     var buffer = {};
     buffer.cursor = { x: 1, y: 3 };
     buffer.ocursor = { x: 2, y: 4 };
-    var n = a.cursor_save(buffer);
+    var n = a.saveCursor(buffer);
     expect(n.ocursor.x).to.be(1);
     expect(n.ocursor.y).to.be(3);
     done();
@@ -36,7 +36,7 @@ describe('terminal buffer actions', function () {
     var buffer = {};
     buffer.cursor = { x: 1, y: 3 };
     buffer.ocursor = { x: 2, y: 4 };
-    var n = a.cursor_restore(buffer);
+    var n = a.restoreCursor(buffer);
     expect(n.cursor.x).to.be(2);
     expect(n.cursor.y).to.be(4);
     done();
--- test/term-buffer.js
+++ test/term-buffer.js
@@ -2,10 +2,71 @@ describe('TermBuffer', function () {
 
   var TermBuffer = require('../index.js').TermBuffer;
 
-  it('can be initialize', function(done) {
-    var t = new TermBuffer(10,20);
-    expect(t).not.to.be(null);
+  it('can be initialized', function(done) {
+    var t = new TermBuffer(80,24);
+    expect(t.getCursor()).to.eql({ x:0, y:0});
+    expect(t.getSize()).to.eql({ cols: 80, rows: 24});
     done();
   });
+
+  it('can move the cursor', function(done) {
+    var t = new TermBuffer(80,24);
+    t.moveCursor(10,20);
+    expect(t.getCursor()).to.eql({ x: 10, y: 20});
+    done();
+  });
+
+  it('can set the cursor', function(done) {
+    var t = new TermBuffer(80,24);
+    t.setCursor(10,20).setCursor(3,3);
+    expect(t.getCursor()).to.eql({ x: 3, y: 3});
+    done();
+  });
+
+  it('can save the cursor', function(done) {
+    var t = new TermBuffer(80,24);
+    t.saveCursor().moveCursor(10,20).restoreCursor();
+    expect(t.getCursor()).to.eql({ x: 0, y: 0});
+    done();
+  });
+
+  it('can get the size', function(done) {
+    var t = new TermBuffer(80,24);
+    var size = t.getSize();
+    expect(size).to.eql({ cols: 80, rows: 24});
+    done();
+  });
+
+  it('can get the cols', function(done) {
+    var t = new TermBuffer(80,24);
+    var cols = t.getCols();
+    expect(cols).to.be(80);
+    done();
+  });
+
+  it('can get the rows', function(done) {
+    var t = new TermBuffer(80,24);
+    var cols = t.getRows();
+    expect(cols).to.be(24);
+    done();
+  });
+
+  it('can backspace', function(done) {
+    var t = new TermBuffer(80,24);
+    t.backspace();
+    expect(t.getCursorX()).to.be(0);
+    expect(t.getCursorY()).to.be(0);
+    done();
+  });
+
+  it('can backspace', function(done) {
+    var t = new TermBuffer(80,24);
+    t.newLine();
+    t.backspace();
+    expect(t.getCursorX()).to.be(79);
+    expect(t.getCursorY()).to.be(0);
+    done();
+  });
+
 
 });
